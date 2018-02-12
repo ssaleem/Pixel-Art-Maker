12d920d3efa0e454ea35e1d11249302bf34956bf
Sara Saleem <ssaleem@Saras-MBP-2.home>
2018-02-12 13:49:11 -0500

    Implement grid using canvas and use javaScript

--- designs.js
+++ designs.js
@@ -1,34 +1,83 @@
-$(function(){
-	var color = $('#colorPicker');
-	var table = $('#pixelCanvas');
 
-	//apply color to cell, when clicked
-	table.on( "click", "td", function( event ) {
-    	event.preventDefault();
-    	$( this ).css( "background-color", color.val() );
-	});
+var formElement =  document.getElementById('sizePicker');
+var widthElement = document.getElementById('inputWidth');
+var heightElement = document.getElementById('inputHeight');
+var width;
+var height;
 
-	// When size is submitted by the user, make grid
-	$('#sizePicker').submit(function(event){
-	  	var width = $('#inputWidth').val();
-	  	var height = $('#inputHeight').val();
-	  	// Delete any existing grid
-	  	table.find('tr').remove();
-	  	for(var row = 1; row <= height; row++){
-	  		table.append("<tr></tr>");
-	  		// console.log(table.children()); //logs <tbody>
-	  		// hence use 'find' to get to <tr>
-	  		for(var col = 1; col <= width;col++){
-	    		table.find('tr').last().append("<td></td>");
-			}
-	    }
-	  	event.preventDefault();
-    });
+var colorElement = document.getElementById('colorPicker');
 
+var canvasDiv = document.getElementById( 'cDiv');
+var canvas;
+var ctx;
+var cellSide = 20;
+const xTranslate = 0.5;
+const yTranslate = 0.5;
+const transMargin = 1;
+
+// optional offset
+var xOffset = 0;
+var yOffset = 0;
+
+
+// event listener for grid size
+formElement.addEventListener('submit', function(event) {
+  //prevent default behaviour
+  event.preventDefault();
+  //extract grid dimensions from form input
+  width = widthElement.value * cellSide + transMargin;
+  height = heightElement.value * cellSide + transMargin;
+  console.log(width + ',' + height);
+  // delete old canvas if any
+  var oldCanvas = document.getElementById('canvas');
+  console.log(oldCanvas);
+  if(oldCanvas){
+    canvasDiv.removeChild(oldCanvas);
+  }
+  // create new canvas
+  canvas = document.createElement("canvas");
+  Object.assign(canvas, {
+  width: width,
+  height: height,
+  id: 'canvas'
+  });
+  //add new canvas to page
+  canvasDiv.append(canvas);
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+    ctx.translate(xTranslate, yTranslate); //to make crisp lines
+  }
+  makeGrid(height,width);
+
+  //event listener to color cells
+  canvas.addEventListener('click', function(event){
+    var x = event.clientX - canvas.offsetLeft;     // Get the horizontal coordinate
+    var y = event.clientY - canvas.offsetTop;     // Get the vertical coordinate
+    // console.log(x + ',' + y);
+    ctx.fillStyle = colorElement.value;
+    ctx.fillRect(x-x%cellSide + 0.5, y - y%cellSide + 0.5, cellSide - 1, cellSide -1);
+  });
 });
 
-	// update paint color on events: {change, input}
-	// redundant function [var color is color value not object]
-	// $('#colorPicker').on('change input',function(){
-	// 	color = $('#colorPicker').val();
-	// });
+
+function makeGrid(height,width){
+  if (ctx) {
+    // clear canvas not needed since canvas is redrawn everytime
+    // ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.moveTo(xOffset, yOffset);
+    ctx.lineTo(xOffset, height * cellSide);
+    ctx.moveTo(xOffset, yOffset);
+    ctx.lineTo(width * cellSide, yOffset);
+
+    for(var row = 0; row < height; row++){
+    	for(var col = 1; col <= width;col++){
+  	    ctx.moveTo(col*cellSide, row*cellSide);
+  	    ctx.lineTo(col*cellSide, (row+1)*cellSide);
+  	    ctx.lineTo((col-1)*cellSide, (row+1)*cellSide);
+    	}
+    }
+    ctx.stroke();
+  }
+}
